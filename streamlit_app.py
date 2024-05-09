0f80f066bd70f36c78f5a999a906f0601eecbc3c
altamash7866 <kanversedemo@gmail.com>
2024-05-09 16:54:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,11 @@ if ingredients_list:
     ingredients_string=' '
 
     for i in ingredients_list:
-        ingredients_string=ingredients_string+i+' '
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	    
+            ingredients_string=ingredients_string+i+' '
+	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-	fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+	    fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 	    
     #st.write(ingredients_string)
 

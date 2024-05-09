0bbb782cd1820dbacd204de84306ba9c2f4a1a56
altamash7866 <kanversedemo@gmail.com>
2024-05-09 17:20:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ session = cnx.session()
 
 
 
-my_dataframe=session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON')
+my_dataframe=session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe,use_container_width=True)
 st.stop()								
 
